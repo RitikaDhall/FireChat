6e46acb40a86d2e66862a4555204ff0219206a49
Ritika Dhall <ritikapdhall@gmail.com>
2020-11-01 23:21:14 +0530

    Set up Firebase & Email/Password log in.

--- FireChat/Controllers/Login/LoginViewController.swift
+++ FireChat/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -122,7 +123,15 @@ class LoginViewController: UIViewController {
             return
         }
         
-        // Firebase log in
+        // MARK: Firebase log in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to login user with email \(email)")
+                return
+            }
+            let user = result.user
+            print("Logged in user: \(user)")
+        })
     }
     
     func alertLoginError() {
--- Podfile
+++ Podfile
@@ -2,9 +2,10 @@
 # platform :ios, '9.0'
 
 target 'FireChat' do
-  # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for FireChat
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
 
 end
